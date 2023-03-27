44181ba965dd2588598d847b6db644205af31187
bobbynagra92 <bobbynagra@hotmail.co.uk>
2023-03-27 19:25:35 +0100

    4.GET/api/articles/article_id - Cleaned Up

--- models/articles.model.js
+++ models/articles.model.js
@@ -4,7 +4,6 @@ function fetchArticle(article_id){
   return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id]).then((result) => {
     const output = result.rows[0];
     if (!output){
-      console.log('in here')
       return Promise.reject({
         status: 404,
         msg: 'No article with that ID',
