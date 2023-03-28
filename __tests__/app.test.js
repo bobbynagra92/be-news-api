67d92cab5378bb1b00c796bf0b54118ad289b323
bobbynagra92 <bobbynagra@hotmail.co.uk>
2023-03-28 10:46:24 +0100

    4.GET/api/articles/article_id - Minor Changes Following Review Comments

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -60,7 +60,7 @@ describe('/api/articles/:article_id', () => {
         expect(article).toMatchObject({
           author: expect.any(String),
           title: expect.any(String),
-          article_id: expect.any(Number),
+          article_id: 5,
           body: expect.any(String),
           topic: expect.any(String),
           created_at: expect.any(String),
