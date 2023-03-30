91a37dce5b653fd00d7c1fd923a93ab71ce5646e
bobbynagra92 <bobbynagra@hotmail.co.uk>
2023-03-30 14:23:37 +0100

    8.PATCH/api/articles/article_id - Ready for Review

--- models/articles.model.js
+++ models/articles.model.js
@@ -59,3 +59,18 @@ exports.addComment = (article_id, comment) => {
   return result.rows[0];
 })
 };
+
+exports.changeVotes = (article_id, votes) => {
+  return db
+    .query(
+      `
+    UPDATE articles SET votes = votes + $1
+    WHERE article_id = $2
+    RETURNING *;
+  `,
+      [votes, article_id]
+    )
+    .then((result) => {
+      return result.rows[0];
+    });
+}
