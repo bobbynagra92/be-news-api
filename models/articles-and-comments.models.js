b409f6323b8888e6785aa304aa4b86b1e6f9f6d2
bobbynagra92 <bobbynagra@hotmail.co.uk>
2023-03-31 19:36:12 +0100

    12.GET/api/articles/article_id(comment_count) - Refactor completed

--- models/articles-and-comments.models.js
+++ models/articles-and-comments.models.js
@@ -3,7 +3,15 @@ const fetchTopics = require('./topics.model');
 
 exports.fetchArticle = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .query(
+      ` SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.body ,articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(comment_id) AS INTEGER) AS comment_count
+      FROM articles
+      LEFT JOIN comments ON articles.article_id = comments.article_id
+      WHERE articles.article_id = $1
+      GROUP BY articles.article_id; 
+      `,
+      [article_id]
+    )
     .then((result) => {
       const output = result.rows[0];
       if (!output) {
